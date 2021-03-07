a1cb0e54091e509b3b8bc8d16d3368b954526c0f
Silzhar <luis.ruizfer@gmail.com>
2021-03-07 11:05:56 +0100

    Add routes

--- models/Samples.js
+++ models/Samples.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose')
 
-
 const samplesSchema = new mongoose.Schema(
-    {
-		date: String,
-		heartStatus: Number,
-		pulse: Number,
-		hasECG: Boolean,
-		anomaly: Boolean,
-		user: Number,
-	},
+  {
+    id: { type: String, required: true },
+    date: { type: String },
+    heartStatus: { type: Number },
+    pulse: { type: Number },
+    hasECG: { type: Boolean },
+    anomaly: { type: Boolean },
+    user: { type: Number },
+  },
+  {
+    timestamps: true,
+  }
 )
 
 const Samples = mongoose.model('Sample', samplesSchema)
